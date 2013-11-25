722adccf632eaec69a48bb6327deaa87e1d3704f
klamkiewicz <icespam@wp.pl>
2013-11-25 14:06:07 +0100

    Newton - done

--- MetodyObliczeniowe2/src/NewtonMethod.java
+++ MetodyObliczeniowe2/src/NewtonMethod.java
@@ -1,4 +1,17 @@
 public class NewtonMethod {
-	
+	static double x = 0;
+	static int iloscKrokow = 1;
+	static double wynik = 1;
 
+	public static int newton(){
+		wynik = wynik - ((Math.pow(wynik, 5) + wynik - 10)/(Math.pow(5*wynik, 4) + 1));
+		if((Startup.finalSolution + Startup.przyblizenie)>=wynik && (Startup.finalSolution-Startup.przyblizenie)<=wynik){
+			return iloscKrokow;
+		}
+		else{
+			iloscKrokow++;
+			newton();
+		}
+		return iloscKrokow;
+	}
 }
--- MetodyObliczeniowe2/src/Startup.java
+++ MetodyObliczeniowe2/src/Startup.java
@@ -1,7 +1,35 @@
+import java.util.Scanner;
+
 public class Startup {
+	public static final int a = -2;
+	public static final int b = 2;
 	public static final double finalSolution = 1.53301;
+	public static double przyblizenie = 0.5;
 	
 	public static void main(String[] args) {
 		
-	}
+		System.out.println("Wprowadz przyblizenie: ");
+		String check;
+		Scanner sc = new Scanner(System.in);
+        do{
+            check = sc.next();
+            if(!isDouble(check)){
+                    System.out.println("Podaj poprawne dane!");
+            }
+        }while(!isDouble(check));
+        przyblizenie = Double.parseDouble(check);
+        
+        System.out.println(NewtonMethod.newton());
+     }
+	
+    public static boolean isDouble(String s) {
+        try {
+        	Double.parseDouble(s);
+        } catch(NumberFormatException e) {
+        	return false;
+        }
+        return true;
+    }
+
+
 }
