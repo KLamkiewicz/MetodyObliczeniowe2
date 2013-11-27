8170208e76ddeb7f27852435cf0828cd63b53737
klamkiewicz <icespam@wp.pl>
2013-11-27 10:46:21 +0100

    Done

--- MetodyObliczeniowe2/src/Startup.java
+++ MetodyObliczeniowe2/src/Startup.java
@@ -4,18 +4,25 @@ public class Startup {
 	public static final double a = -2;
 	public static final double b = 2;
 	public static final double finalSolution = 1.53301;
-	public static double przyblizenie = 0.5;
+	public static double przyblizenie;
 	
 	public static void main(String[] args) {
 		
-		System.out.println("Wprowadz przyblizenie: ");
+		System.out.println("Wprowadz przyblizenie o wartosci pomiedzy 0 a 1: ");
 		String check;
 		Scanner sc = new Scanner(System.in);
         do{
             check = sc.next();
             if(!isDouble(check)){
                     System.out.println("Podaj poprawne dane!");
+            }else{
+       
+            	if(Double.parseDouble(check)>=1 || Double.parseDouble(check)<=0){
+            		System.out.println("Nieprawidlowe dane, przyblizenie ma zawierac sie pomiedzy 0 a 1");
+            		check = "s";
+            	}
             }
+            
         }while(!isDouble(check));
         przyblizenie = Double.parseDouble(check);
         
